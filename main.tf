61bf3991a1e17e0a31fd8ac0b1ddb80ac0cc16ca
thejaswara <thejaswarababu@gmai.com>
2025-04-19 12:16:31 +0530

    table association

--- main.tf
+++ main.tf
@@ -150,3 +150,26 @@ destination_cidr_block = "0.0.0.0/0"
 nat_gateway_id = aws_nat_gateway.main.id
   
 }
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr)
+  route_table_id = aws_route_table.public.id
+  subnet_id = element(aws_subnet.public[*].id, count.index)
+  
+}
+
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidr)
+  route_table_id = aws_route_table.private.id
+  subnet_id = element(aws_subnet.private[*].id, count.index)
+  
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidr)
+  route_table_id = aws_route_table.databse.id
+  subnet_id = element(aws_subnet.databse[*].id, count.index)
+  
+}
+
